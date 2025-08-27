bb21a3a794d657bd07eba8163aebda5cabfbc862
Dany-crtl <danielarcila1138700@correo.itm.edu.com>
2025-08-27 03:05:28 -0500

    intento1

---
+++ Solution2/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var numberString = string.Empty
+do
+{
+    Console.Write("Ingrese Numero entero o la plabra salir: ");
+    var numberString = Console.ReadLine();
+    if (numberString.ToLower() == "salir"
+    {
+       continue;
+    }
+ 
+    var numberint = 0;
+    if (int.TryParse(numberString, out numberint));
+    if (numberint % 2 == 0)
+    {
+        Console.WriteLine($"El numero {numberString}, es par");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {numberString}, es impar");
+    }
+    else
+    {
+        Console.WriteLine($"El numero {numberString}, ingresado no es un numero entero");
+    }
+} while (numberString != "S");
+   Console.WriteLine("GAME OVER.").
